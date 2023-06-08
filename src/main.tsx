694ea2c4089742f4ac1d676a09862e37059e2ecb
unitax13 <61455951+unitax13@users.noreply.github.com>
2023-06-08 23:58:09 +0200

    there's a board

--- src/main.tsx
+++ src/main.tsx
@@ -1,34 +1,37 @@
-const canvas = document.getElementById('canvas') as HTMLCanvasElement;
-const increaseBtn = document.getElementById('increase') as HTMLButtonElement;
-const decreaseBtn = document.getElementById('decrease') as HTMLButtonElement;
-const sizeEL = document.getElementById('size') as HTMLSpanElement;
-const colorEl = document.getElementById('color') as HTMLInputElement;
-const clearEl = document.getElementById('clear') as HTMLButtonElement;
+const canvas = document.getElementById("canvas") as HTMLCanvasElement;
+const increaseBtn = document.getElementById("increase") as HTMLButtonElement;
+const decreaseBtn = document.getElementById("decrease") as HTMLButtonElement;
+const sizeEL = document.getElementById("size") as HTMLSpanElement;
+const colorEl = document.getElementById("color") as HTMLInputElement;
+const clearEl = document.getElementById("clear") as HTMLButtonElement;
 
-const ctx = canvas.getContext('2d');
+const ctx = canvas.getContext("2d");
+
+const canvasWidth = 800;
+const canvasHeight = 700;
 
 let size = 10;
 let isPressed = false;
-colorEl.value = 'black';
+colorEl.value = "black";
 let color = colorEl.value;
 let x: number | undefined;
 let y: number | undefined;
 
-canvas.addEventListener('mousedown', (e) => {
+canvas.addEventListener("mousedown", (e) => {
   isPressed = true;
 
   x = e.offsetX;
   y = e.offsetY;
 });
 
-document.addEventListener('mouseup', () => {
+document.addEventListener("mouseup", () => {
   isPressed = false;
 
   x = undefined;
   y = undefined;
 });
 
-canvas.addEventListener('mousemove', (e) => {
+canvas.addEventListener("mousemove", (e) => {
   if (isPressed && x !== undefined && y !== undefined) {
     const x2 = e.offsetX;
     const y2 = e.offsetY;
@@ -61,7 +64,7 @@ function updateSizeOnScreen() {
   sizeEL.innerText = size.toString();
 }
 
-increaseBtn.addEventListener('click', () => {
+increaseBtn.addEventListener("click", () => {
   size += 5;
 
   if (size > 50) {
@@ -71,7 +74,7 @@ increaseBtn.addEventListener('click', () => {
   updateSizeOnScreen();
 });
 
-decreaseBtn.addEventListener('click', () => {
+decreaseBtn.addEventListener("click", () => {
   size -= 5;
 
   if (size < 5) {
@@ -81,19 +84,17 @@ decreaseBtn.addEventListener('click', () => {
   updateSizeOnScreen();
 });
 
-colorEl.addEventListener('change', (e) => (color = e.target.value));
+colorEl.addEventListener("change", (e) => (color = e.target.value));
 
-clearEl.addEventListener('click', () =>
+clearEl.addEventListener("click", () =>
   ctx?.clearRect(0, 0, canvas.width, canvas.height)
 );
-
-
 
 enum FieldType {
   Empty,
   Road1,
   Urban,
-  Industrial
+  Industrial,
 }
 
 const numRows = 100;
@@ -111,37 +112,71 @@ for (let i = 0; i < numRows; i++) {
   }
 }
 
-
 fieldArray[10][10] = FieldType.Road1;
 fieldArray[20][20] = FieldType.Industrial;
 
-ctx!.fillStyle = 'red';
-ctx?.fillRect(0,0,canvas.width,canvas.height);
+ctx!.fillStyle = "red";
+ctx?.fillRect(0, 0, canvas.width, canvas.height);
 
-const urbanIsOn = true
+const urbanIsOn = true;
 const industryIsOn = true;
 const roadsIsOn = true;
 const fieldWidth = 6;
 const fieldHeight = 6;
-let cameraX = 0
-let cameraY = 0
-let cameraScale =1;
+let cameraX = 0;
+let cameraY = 0;
+let cameraScale = 1;
+let gridOpacity = 10;
 
-for (let x = 0; x < numRows; x++) {
-  for (let y = 0; y < numColumns; y++) {
-      
-      let type:FieldType = fieldArray[x][y];
+drawMainGrid();
+drawGridOverlay();
+
+function drawMainGrid() {
+  for (let x = 0; x < numRows; x++) {
+    for (let y = 0; y < numColumns; y++) {
+      let type: FieldType = fieldArray[x][y];
       if (type == FieldType.Urban && urbanIsOn) {
-          color = 'green';
+        color = "green";
       } else if (type == FieldType.Industrial && industryIsOn) {
-          color = 'purple';
+        color = "#666622";
       } else if (type == FieldType.Road1 && roadsIsOn) {
-          color = 'black';
-      } else { // EMPTY
-          color = 'yellow'
+        color = "#111111";
+      } else {
+        // EMPTY
+        color = "#ddeeee";
       }
       ctx!.fillStyle = color;
-      ctx!.fillRect(fieldWidth * 1 * x - cameraX, fieldHeight * cameraScale * y - cameraY, fieldWidth * cameraScale, fieldHeight * cameraScale);
-
+      ctx!.fillRect(
+        fieldWidth * 1 * x - cameraX,
+        fieldHeight * cameraScale * y - cameraY,
+        fieldWidth * cameraScale,
+        fieldHeight * cameraScale
+      );
+    }
   }
+}
+
+function drawGridOverlay() {
+  console.log("drawing grid overlay");
+  ctx!.strokeStyle = "#111111";
+  for (let x = 0; x <= numRows; x++) {
+    if (x % 10 == 0) {
+      ctx!.lineWidth = (2 * gridOpacity) / 100;
+    } else {
+      ctx!.lineWidth = (0.5 * gridOpacity) / 100;
+    }
+
+    ctx!.moveTo(x * fieldWidth * cameraScale - cameraX, -1 - cameraY);
+    ctx!.lineTo(
+      x * fieldWidth * cameraScale - cameraX,
+      canvasHeight * cameraScale - cameraY
+    );
+
+    ctx!.moveTo(-1 - cameraX, x * fieldWidth * cameraScale - cameraY);
+    ctx!.lineTo(
+      canvasWidth * cameraScale - cameraX,
+      x * fieldWidth * cameraScale - cameraY
+    );
+  }
+  ctx!.stroke();
 }
